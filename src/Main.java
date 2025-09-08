516c88675ccae043a909f5568b1f0cf7562f52f4
Nicolas <n0252884@gmail.com>
2025-09-07 23:25:22 -0500

    comit inical

--- src/Main.java
+++ src/Main.java
@@ -5,6 +5,6 @@ public class Main {
         CuentaBancaria cuenta = new CuentaBancaria();
         cuenta.depositar(500);
         cuenta.retirar(200);
-        System.out.println("Saldo: " + cuenta.getSaldo()); // debe imprimir 300.0
+        System.out.println("Saldo: " + cuenta.getSaldo());
     }
 }
